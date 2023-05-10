b045636e19d08ff56d2e11128c28fa35052d55ff
SujinRo <shtnwls1111@ewhain.net>
2023-05-11 01:45:12 +0900

    design: landing 디자인 수정

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,8 +1,27 @@
 import Lottie from 'react-lottie-player';
 import lottieJson from '../../../public/Lottie/29313-netflix-logo-swoop.json';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+const Content = styled.div`
+display: flex;
+height: 46rem;
+width: 23.4375rem;
+align-items: center;
+margin-bottom: 3.261875rem;
+`;
 
 export default function LandingPage() {
     return (
-      <Lottie loop animationData={lottieJson} play style={{width: 150, height: 150}}/>
+      <Wrapper>
+       <Content>
+      <Lottie loop animationData={lottieJson} play/>
+      </Content>
+      </Wrapper>
     )
   }
