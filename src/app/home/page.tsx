4bb0f75b713f5b98915f33e64f0f80fd00126dc2
Yoojin Shin <yjshin1003@naver.com>
2023-05-12 04:59:03 +0900

    fix: Home페이지 타입 및 use() 적용

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,4 @@
+import { use } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import {
   getUpcomingMovies,
@@ -5,26 +6,36 @@ import {
   getHorrorMovies,
 } from '../../service/movies';
 import { getAnimations, getTopRatedTvShows } from '../../service/tvshows';
+import { IMovie, ITvShow } from '@/interface/interface';
+import Image from 'next/image';
 
 export default function HomePage() {
-  const { data: upcomingMovies } = useQuery(['upcomingMovies'], () =>
-    getUpcomingMovies(),
+  const { data: upcomingMovies } = useQuery<IMovie[]>(['upcomingMovies'], () =>
+    use(getUpcomingMovies()),
   );
-  const { data: nowPlayingMovies } = useQuery(['nowPlayingMovies'], () =>
-    getNowPlayingMovies(),
+  const { data: nowPlayingMovies } = useQuery<IMovie[]>(
+    ['nowPlayingMovies'],
+    () => use(getNowPlayingMovies()),
   );
-  const { data: horrorMovies } = useQuery(['horrorMovies'], () =>
-    getHorrorMovies(),
+  const { data: horrorMovies } = useQuery<IMovie[]>(['horrorMovies'], () =>
+    use(getHorrorMovies()),
   );
-  const { data: animations } = useQuery(['animations'], () => getAnimations());
-  const { data: topRatedTvShows } = useQuery(['topRatedTvShows'], () =>
-    getTopRatedTvShows(),
+  const { data: animations } = useQuery<ITvShow[]>(['animations'], () =>
+    use(getAnimations()),
+  );
+  const { data: topRatedTvShows } = useQuery<ITvShow[]>(
+    ['topRatedTvShows'],
+    () => use(getTopRatedTvShows()),
   );
 
   return (
     <ul>
-      {upcomingMovies.map(video => (
-        <img key={video.id} src={} />
+      {upcomingMovies!.map(video => (
+        <Image
+          key={video.id}
+          src={`https://image.tmdb.org/t/p/original${video.poster_path}`}
+          alt="videoImg"
+        />
       ))}
     </ul>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <QueryClientProvider client={queryClient}>
+        <body className={inter.className}>{children}</body>
+      </QueryClientProvider>
     </html>
   );
 }
