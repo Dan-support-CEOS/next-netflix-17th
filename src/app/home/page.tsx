023aed2cad6de63d85544f89655abb7b16bc9c92
Yoojin Shin <yjshin1003@naver.com>
2023-05-12 02:14:58 +0900

    feat: Home페이지에 API 데이터 가져오기

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+import {
+  getUpcomingMovies,
+  getNowPlayingMovies,
+  getHorrorMovies,
+} from '../../service/movies';
+import { getAnimations, getTopRatedTvShows } from '../../service/tvshows';
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const { data: upcomingMovies } = useQuery(['upcomingMovies'], () =>
+    getUpcomingMovies(),
+  );
+  const { data: nowPlayingMovies } = useQuery(['nowPlayingMovies'], () =>
+    getNowPlayingMovies(),
+  );
+  const { data: horrorMovies } = useQuery(['horrorMovies'], () =>
+    getHorrorMovies(),
+  );
+  const { data: animations } = useQuery(['animations'], () => getAnimations());
+  const { data: topRatedTvShows } = useQuery(['topRatedTvShows'], () =>
+    getTopRatedTvShows(),
+  );
+
+  return (
+    <ul>
+      {upcomingMovies.map(video => (
+        <img key={video.id} src={} />
+      ))}
+    </ul>
+  );
 }
