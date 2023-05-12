8b961cd3eb91b016754effe1532a6373198427b5
SujinRo <shtnwls1111@ewhain.net>
2023-05-12 23:29:01 +0900

    feat: random image

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,8 +8,28 @@ import {
 } from '../../service/movies';
 import { getAnimations, getTopRatedTvShows } from '../../service/tvshows';
 import { IMovie, ITvShow } from '@/interface/interface';
+import styled from 'styled-components';
+
+const Wrapper = styled.ul`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+const Content = styled.div`
+display: flex;
+height: 46rem;
+width: 23.4375rem;
+`;
+
+const Image = styled.img`
+height:25.938rem;
+width: 100%;
+`;
+
 
 export default function HomePage() {
+
   const { data: upcomingMovies } = useQuery<IMovie[]>(
     ['upcomingMovies'],
     getUpcomingMovies,
@@ -30,10 +50,15 @@ export default function HomePage() {
     ['topRatedTvShows'],
     getTopRatedTvShows,
   );
-
-  return (
-    <ul>
-      {nowPlayingMovies && nowPlayingMovies!.map(video => <p>{video.title}</p>)}
-    </ul>
+  
+ if(nowPlayingMovies && nowPlayingMovies!){
+    const random = Math.floor(Math.random() * nowPlayingMovies.length);
+ 
+    return (
+    <Wrapper>
+     <Content>
+        <Image src={`https://image.tmdb.org/t/p/original${nowPlayingMovies[random].poster_path}`}/>
+      </Content>
+    </Wrapper>
   );
-}
+}}
